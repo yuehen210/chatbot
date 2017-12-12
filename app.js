5454edb56f9c57f7e48866ee447c3363bf586cdc
yuehen210 <yuehen210@yahoo.co.jp>
2017-12-12 11:50:42 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+// region ***** require *****
+const restify = require('restify');
+const builder = require('botbuilder');
+const scheduler = require('node-schedule');
+const request = require('superagent');
+
+// endregion
+
+//region ***** Server セットアップ *****/
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+    console.log("Server Start");
+});
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID, // MBFPortalに作成したボットのID
+    appPassword: process.env.MICROSOFT_APP_PASSWORD // MBFPortalに作成したボットのPassword
+});
+
+server.post('/', connector.listen()); // 例：https://xxx.co.jp/
+//endregion
+
+//region ***** Bot セットアップ ***** /
+var bot = module.exports = new builder.UniversalBot(connector, [
+    (session, args, next) => {
+        session.send(session.message.text);
+    }
+]);
